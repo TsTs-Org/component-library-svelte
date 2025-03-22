0b5a1a1ff463afb7f615dd2a30ce2d0bdd8fba14
Tom Schneider <05.tom.schneider@gmail.com>
2025-03-22 13:10:28 +0100

    use bits-ui and scss

---
+++ src/lib/Button.svelte
@@ -0,0 +1,12 @@
+<svelte:options runes={true}/><!-- is this needed? -->
+
+<script lang="ts">
+    import { Button } from "bits-ui";
+    import "../styles/button.scss"
+</script>
+
+<Button.Root
+  class="red"
+>
+  some content
+</Button.Root>
