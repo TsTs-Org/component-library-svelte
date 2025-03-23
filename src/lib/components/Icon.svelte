d5983603d49f16f798fbf7817a0b09b8ebaf7d54
TimSalokat <Tim.salokat@outlook.com>
2025-03-23 10:24:48 +0100

    Added Icon component and adjusted depending code

---
+++ src/lib/components/Icon.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+	import type { Snippet } from "svelte";
+	import type { SVGAttributes } from "svelte/elements";
+
+	type Size = 's' | 'm' | 'l';
+    type Props = { size?: Size; icon?: Snippet; children: Snippet } & SVGAttributes<any>;
+
+    let {
+        size = 'm',
+        icon,
+        children,
+        ...restProps
+    }: Props = $props();
+</script>
+
+<svg class={[size]} {...restProps} xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960">
+    {@render children?.()}
+</svg>
+
+<style lang="scss">
+svg {
+    fill: var(--text-color);
+    &.s {
+        height: 1.25rem;
+        width: 1.25rem;
+    }
+    &.m {
+        height: 1.75rem;
+        width: 1.75rem;
+    }
+    &.l {
+        height: 2.5rem;
+        width: 2.5rem;
+    }
+}
+</style>
--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,12 +1,13 @@
-// Reexport your entry components here
 import Button from "./components/Button.svelte";
 import Link from "./components/Link.svelte";
 import NavigationBar from "./components/NavigationBar.svelte";
 import Input from "./components/Input.svelte";
+import Icon from "./components/Icon.svelte";
 
 export { 
     Button,
     Link,
     NavigationBar,
     Input,
+    Icon
 }
