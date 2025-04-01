b29397b49cb237d0eeecd4224268de05dcf2dfa1
tim-salokat <tim.salokat@otto.de>
2025-04-01 16:59:01 +0200

    WIP Table

---
+++ src/lib/components/Table/TableHead.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import type { Snippet } from "svelte";
+
+	type Props = {
+		children: Snippet;
+	};
+
+	let { children }: Props = $props();
+</script>
+
+<th>
+	{@render children?.()}
+</th>
+
+<style>
+	th {
+		text-align: left;
+		padding: var(--padding-s);
+		font-weight: 600;
+		font-size: 0.9rem;
+		color: var(--text-color-muted);
+	}
+</style>
---
+++ src/lib/components/Table/TableRow.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import type { Snippet } from "svelte";
+
+	type Props = {
+		children: Snippet;
+	};
+
+	let { children }: Props = $props();
+</script>
+
+<tr>
+	{@render children?.()}
+</tr>
+
+<style lang="scss">
+	tr {
+		border-bottom: thin solid var(--border-color);
+		&:hover {
+			background-color: var(--neutral-hover-color);
+		}
+	}
+</style>
