148c74827550c8616259cbaa24672bf20bdde978
tim-salokat <tim.salokat@otto.de>
2025-04-04 14:40:49 +0200

    Implemented Multiselect

---
+++ src/lib/components/Multiselect/Multiselect.svelte
@@ -0,0 +1,160 @@
+<script lang="ts">
+	import { onMount, setContext } from "svelte";
+	import type { HTMLAttributes } from "svelte/elements";
+	import Icon from "../Icon.svelte";
+	import { slide } from "svelte/transition";
+	import { writable, type Writable } from "svelte/store";
+
+	type Variant = "ghost" | "bordered";
+
+	type Props = {
+		onchange?: (value: Array<Option>) => void;
+		value?: Array<Option>;
+		label?: string;
+		description?: string;
+		initial?: Array<Option>;
+		variant?: Variant;
+		placeholder: string;
+	} & HTMLAttributes<any>;
+
+	type Option = string | number;
+
+	export type MultiselectCtx = {
+		selected: Writable<Array<Option>>;
+	};
+
+	let {
+		onchange = () => {},
+		value = $bindable(),
+		label,
+		description,
+		initial = [],
+		variant = "bordered",
+		placeholder,
+		children,
+		...restProps
+	}: Props = $props();
+
+	let open = $state(false);
+	// let selectedOptions: Array<Option> = $state([]);
+
+	const ctx = {
+		selected: writable(initial),
+	};
+
+	setContext("MultiselectCtx", ctx);
+	let initialized = false;
+
+	onMount(() => {
+		document.addEventListener("click", (event: MouseEvent) => {
+			if (event.target && !event.target.closest(".Multiselect") && open) {
+				open = false;
+			}
+		});
+		return ctx.selected.subscribe((x) => {
+			if (!initialized) {
+				initialized = true;
+				return; // Skip the first run
+			}
+			value = x;
+			onchange(x);
+		});
+	});
+</script>
+
+<div class="Multiselect">
+	{#if label}
+		<label>{label}</label>
+	{/if}
+
+	<button
+		class={["Trigger", variant]}
+		{...restProps}
+		onclick={() => (open = !open)}
+	>
+		<span style="color: var(--text-color)">{placeholder}</span>
+
+		<Icon
+			iconName={open ? "arrowUp" : "arrowDown"}
+			size="s"
+			style="fill: var(--text-color-muted)"
+		/>
+	</button>
+
+	{#if open}
+		<div
+			class="Portal"
+			transition:slide={{ duration: 190 }}
+		>
+			{@render children?.()}
+		</div>
+	{/if}
+
+	{#if description}
+		<p>{description}</p>
+	{/if}
+</div>
+
+<!--
+@component
+## Props
+@prop {string} [label] - The label for the select input.
+@prop {string} [description] - The description for the select input.
+@prop {string} [placeholder] - The placeholder text for the select input.
+@prop {string} [variant] - The variant of the select input. Can be "ghost" or "bordered".
+@prop {function} [onchange] - The function to call when the select input changes.
+@prop {string} [value] - The value of the select input.
+-->
+
+<style lang="scss">
+	.Multiselect {
+		position: relative;
+	}
+
+	label {
+		font-size: 0.925rem;
+		font-weight: 600;
+		margin-left: var(--padding-xs);
+	}
+	p {
+		font-size: 0.75rem;
+		font-weight: 600;
+		color: var(--text-color-muted);
+		margin-left: var(--padding-xs);
+	}
+	.Trigger {
+		background-color: transparent;
+		outline: none;
+		width: 100%;
+		box-sizing: border-box;
+		height: min-content;
+		text-align: left;
+		display: flex;
+		align-items: center;
+		justify-content: space-between;
+		color: var(--text-color);
+		border-radius: var(--border-radius-s);
+		padding: var(--padding-m);
+		margin-block: var(--padding-xs);
+		&.bordered {
+			border: 1px solid var(--border-color);
+		}
+		&.ghost {
+			border: none;
+		}
+	}
+	button:focus {
+		border-color: var(--primary-color);
+	}
+	.Portal {
+		z-index: 500;
+		position: absolute;
+		overflow: hidden;
+		background-color: var(--foreground-color);
+		width: 100%;
+		min-width: fit-content;
+		box-sizing: border-box;
+		border-radius: var(--border-radius-s);
+		border: 1px solid var(--border-color);
+	}
+</style>
---
+++ src/lib/components/Multiselect/MultiselectItem.svelte
@@ -0,0 +1,95 @@
+<script lang="ts">
+	import { getContext, onMount, type Snippet } from "svelte";
+	// import Icon from "../Icon.svelte";
+	import type { MultiselectCtx } from "./Multiselect.svelte";
+	import type { HTMLAttributes } from "svelte/elements";
+	import Checkbox from "../Checkbox.svelte";
+
+	type Variant = "default" | "ghost";
+
+	type Props = {
+		onchange?: (newValue: boolean) => void;
+		value: string | number;
+		label: string;
+		variant?: Variant;
+		children?: Snippet;
+	} & HTMLAttributes<any>;
+
+	let {
+		onchange = () => {},
+		value,
+		label,
+		variant = "default",
+		children,
+		...restProps
+	}: Props = $props();
+
+	const ctx: MultiselectCtx = getContext("MultiselectCtx");
+	const selected = ctx.selected;
+
+	let isActive = $state(false);
+	onMount(() => {
+		return selected.subscribe((x) => {
+			isActive = x.includes(self.value);
+		});
+	});
+
+	function swapSelected() {
+		selected.update((curr) => {
+			if (curr.includes(self.value)) {
+				return curr.filter((val) => val != self.value);
+			} else {
+				curr.push(self.value);
+			}
+			return curr;
+		});
+	}
+
+	const self = { value, label };
+</script>
+
+<div
+	class={variant + " SelectItem"}
+	class:isActive
+	{...restProps}
+>
+	<Checkbox
+		label={self.label}
+		bind:checked={isActive}
+		onchange={() => {
+			onchange(isActive);
+			swapSelected();
+		}}
+	/>
+</div>
+
+<!--
+@component
+## Props
+@prop {string} value - The value of the item.
+@prop {string} label - The label of the item.
+@prop {string} variant - The variant of the item. Can be "default" or "ghost". Default is "default".
+-->
+
+<style lang="scss">
+	.SelectItem {
+		display: flex;
+		box-sizing: border-box;
+		border-bottom: thin solid var(--border-color);
+		width: 100%;
+		padding: var(--padding-s);
+		&:hover {
+			background-color: var(--neutral-hover-color);
+		}
+		// &.isActive {
+		// 	background-color: var(--hover-color);
+		// }
+		&.ghost {
+			background-color: transparent;
+			border: none;
+		}
+		&:last-child {
+			border: none;
+		}
+	}
+</style>
