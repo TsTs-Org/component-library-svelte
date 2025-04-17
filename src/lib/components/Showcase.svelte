f1d1920f4ef5d6b6a2074fdba4b5fdf0973f2b95
tim-salokat <tim.salokat@otto.de>
2025-04-17 15:29:48 +0200

    Fix: Removed local light mode from showcase as theming works different now

--- src/lib/components/Showcase.svelte
+++ src/lib/components/Showcase.svelte
@@ -1,34 +1,14 @@
 <script lang="ts">
-	import { theme } from "$lib/utils/themify.svelte.js";
 	import Card from "./Card.svelte";
-	import Icon from "./Icon.svelte";
 	let { name, children } = $props();
-	let localLightMode = $state($theme.lightMode);
-	const setMode = () => {
-		localLightMode = !localLightMode;
-	};
-	theme.subscribe((x) => {
-		localLightMode = x.lightMode;
-	});
 </script>
 
-<div class={localLightMode ? "light-mode" : "dark-mode"}>
+<div>
 	<Card
 		size="l"
 		transparent
 	>
 		{#snippet title()}{name}{/snippet}
-		{#snippet iconRight(size)}
-			<button
-				aria-label="Toggle light/dark mode"
-				onclick={() => setMode()}
-			>
-				<Icon
-					iconName={localLightMode ? "sun" : "moon"}
-					{size}
-				></Icon>
-			</button>
-		{/snippet}
 		<div class="content-wrapper">{@render children?.()}</div>
 	</Card>
 </div>
