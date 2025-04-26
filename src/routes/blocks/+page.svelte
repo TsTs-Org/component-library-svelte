72d7cf42018ddd787f9f1f592c4a7e43895ca5c6
TimSalokat <Tim.salokat@outlook.com>
2025-04-26 23:34:32 +0200

    Fix: some imports were declared multiple times due to wrong merge conflict

--- src/routes/blocks/+page.svelte
+++ src/routes/blocks/+page.svelte
@@ -11,10 +11,6 @@
 	import TabContent from "$lib/components/Tabs/TabContent.svelte";
 	import AutoGenerateTriggers from "$lib/components/Tabs/AutoGenerateTriggers.svelte";
 	import DynamicHorizontalTabs from "$lib/blocks/Tabs/DynamicHorizontalTabsBlock.svelte";
-	import Tabs from "$lib/components/Tabs/Tabs.svelte";
-	import TabContent from "$lib/components/Tabs/TabContent.svelte";
-	import AutoGenerateTriggers from "$lib/components/Tabs/AutoGenerateTriggers.svelte";
-	import DynamicHorizontalTabs from "$lib/blocks/Tabs/DynamicHorizontalTabsBlock.svelte";
 	let sidebarClosed = $state(false);
 </script>
 
