cd15319736f6ce7bee40b98debda09104340e4bf
TimSalokat <Tim.salokat@outlook.com>
2025-04-26 23:27:45 +0200

    Removed dangling dublicate from merge conflict

--- src/routes/blocks/+page.svelte
+++ src/routes/blocks/+page.svelte
@@ -70,6 +70,8 @@
 
 			<h2 style="margin: auto">Main Content</h2>
 		</Layout>
+	</div>
+	<div class="mock-block">
 		<div style="padding: .5rem">
 			<Tabs defaultTab="tab1">
 				<div style="display: flex">
@@ -95,62 +97,6 @@
 				<TabContent tabIdentifier="tab4">this is tab 4</TabContent>
 			</Tabs>
 		</div>
-	</div>
-	<div class="mock-block">
-		<Layout style="height: 100%">
-			{#snippet topnav()}
-				<Navigationbar variant="glass">
-					{#snippet left()}
-						<Button
-							variant="ghost"
-							onclick={() => (sidebarClosed = !sidebarClosed)}
-						>
-							{#snippet icon()}
-								<Icon iconName={sidebarClosed ? "sun" : "moon"} />
-							{/snippet}
-						</Button>
-						<NavigationbarItem href="#1">Home</NavigationbarItem>
-						<NavigationbarItem href="#2">Blocks</NavigationbarItem>
-						<NavigationbarItem href="#3">Components</NavigationbarItem>
-					{/snippet}
-				</Navigationbar>
-			{/snippet}
-
-			{#snippet leftSidebar()}
-				<Sidebar {sidebarClosed}>
-					{#snippet sidebarHeader(collapsed)}
-						{#if !collapsed()}
-							<BaseChart
-								chartType="line"
-								displayTooltip
-								yGrid
-								yAxis
-								xAxis
-								labels={["January", "February", "March", "May", "June"]}
-								data={[
-									{
-										title: "DataOne",
-										data: [12, 19, 5, 5, 12, 15],
-									},
-									{
-										title: "DataTwo",
-										data: [2, 8, 2, 11, 7, 4],
-									},
-								]}
-							></BaseChart>
-						{/if}
-					{/snippet}
-					{#each ["Dashboard", "Lifecycle", "Analytics", "Projects"] as name}
-						<SidebarItem size="s">{name}</SidebarItem>
-					{/each}
-					{#snippet sidebarFooter()}
-						<SidebarItem>Logout</SidebarItem>
-					{/snippet}
-				</Sidebar>
-			{/snippet}
-
-			<h2 style="margin: auto">Main Content</h2>
-		</Layout>
 	</div>
 	<div class="mock-block">
 		<SimpleLogin
