6709b70f0afa0fda0a9e364318cf075f660d845e
TimSalokat <Tim.salokat@outlook.com>
2025-04-27 09:49:09 +0200

    [skip ci] Added new components to exports

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -2,10 +2,13 @@
 // ------ Export Components ------
 import Accordion from "./components/Accordion/Accordion.svelte";
 import AccordionItem from "./components/Accordion/AccordionItem.svelte";
+import BaseChart from "./components/Chart/BaseChart.svelte";
+import RoundChart from "./components/Chart/RoundChart.svelte";
 import Contextcontainer from "./components/Contextcontainer/Contextcontainer.svelte";
 import Contextmenu from "./components/Contextcontainer/Contextmenu.svelte";
 import ContextmenuItem from "./components/Contextcontainer/ContextmenuItem.svelte";
 import Dropdown from "./components/Dropdown/Dropdown.svelte";
+import Layout from "../routes/+layout.svelte";
 import Multiselect from "./components/Multiselect/Multiselect.svelte";
 import MultiselectItem from "./components/Multiselect/MultiselectItem.svelte";
 import Navigationbar from "./components/Navigationbar/Navigationbar.svelte";
@@ -14,12 +17,18 @@ import Radiogroup from "./components/Radio/Radiogroup.svelte";
 import RadioItem from "./components/Radio/RadioItem.svelte";
 import Select from "./components/Select/Select.svelte";
 import SelectItem from "./components/Select/SelectItem.svelte";
+import Sidebar from "./components/Sideabar/Sidebar.svelte";
+import SidebarItem from "./components/Sideabar/SidebarItem.svelte";
 import Table from "./components/Table/Table.svelte";
 import TableBody from "./components/Table/TableBody.svelte";
 import TableCell from "./components/Table/TableCell.svelte";
 import TableFooter from "./components/Table/TableFooter.svelte";
 import TableHead from "./components/Table/TableHead.svelte";
 import TableRow from "./components/Table/TableRow.svelte";
+import AutoGenerateTriggers from "./components/Tabs/AutoGenerateTriggers.svelte";
+import TabContent from "./components/Tabs/TabContent.svelte";
+import Tabs from "./components/Tabs/Tabs.svelte";
+import TabTrigger from "./components/Tabs/TabTrigger.svelte";
 
 import Button from "./components/Button.svelte";
 import Card from "./components/Card.svelte";
@@ -27,6 +36,8 @@ import Checkbox from "./components/Checkbox.svelte";
 import Icon from "./components/Icon.svelte";
 import Input from "./components/Input.svelte";
 import Loader from "./components/Loader.svelte";
+import OverflowContainer from "./components/OverflowContainer.svelte";
+import Popover from "./components/Popover.svelte";
 import ScrollContainer from "./components/ScrollContainer.svelte";
 import Seperator from "./components/Seperator.svelte";
 import Themify from "./components/Themify.svelte";
@@ -34,10 +45,13 @@ import Themify from "./components/Themify.svelte";
 export { 
     Accordion,
     AccordionItem,
+    BaseChart,
+    RoundChart,
     Contextcontainer,
     Contextmenu,
     ContextmenuItem,
     Dropdown,
+    Layout,
     Multiselect,
     MultiselectItem,
     Navigationbar,
@@ -46,12 +60,18 @@ export {
     RadioItem,
     Select,
     SelectItem,
+    Sidebar,
+    SidebarItem,
     Table,
     TableBody,
     TableCell,
     TableFooter,
     TableHead,
     TableRow,
+    AutoGenerateTriggers,
+    TabContent,
+    Tabs,
+    TabTrigger,
 
     Button,
     Card,
@@ -59,6 +79,8 @@ export {
     Icon,
     Input,
     Loader,
+    OverflowContainer,
+    Popover,
     ScrollContainer,
     Seperator,
     Themify,
