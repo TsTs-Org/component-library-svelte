42ac46f50db83fceb6c655467d494c770815b96e
TimSalokat <Tim.salokat@outlook.com>
2025-08-28 14:54:03 +0300

    fixed toms shitty script with the use of almighty ai

--- scripts/get_bigger_version_number.sh
+++ scripts/get_bigger_version_number.sh
@@ -1,29 +1,42 @@
-if [[ $1 == $2 ]]
-    then echo $1
-    exit 0
-fi
+#!/bin/bash
 
-# replaces "." with " " and therefore forming array of identifiers (-> numbers inbetween dots)
-IDENTIFIERS_VERSION_1=(${1//./ })
-IDENTIFIERS_VERSION_2=(${2//./ })
-
-# TODO: this won't work for comparing versions that have different length
-if [[ ${#IDENTIFIERS_VERSION_1} -ne ${#IDENTIFIERS_VERSION_2} ]]; then
+# Check if both arguments are provided
+if [[ -z "$1" || -z "$2" ]]; then
+    echo "Usage: $0 version1 version2"
     exit 1
 fi
 
-AMOUNT_OF_IDENTIFIERS=${#IDENTIFIERS_VERSION_1[@]}
+# If versions are equal, print either one and exit
+if [[ "$1" == "$2" ]]; then
+    echo "$1"
+    exit 0
+fi
 
-# return if one version number has a higher identifier that the other
-for ((i = 0; i<$AMOUNT_OF_IDENTIFIERS; i++)); do
-    if (( ${IDENTIFIERS_VERSION_1[$i]} > ${IDENTIFIERS_VERSION_2[$i]} )); then
-        echo $1
+# Replace "." with space to split into array elements
+IFS='.' read -r -a IDENTIFIERS_VERSION_1 <<< "$1"
+IFS='.' read -r -a IDENTIFIERS_VERSION_2 <<< "$2"
+
+# Determine the longest version length to compare all parts
+MAX_LEN=${#IDENTIFIERS_VERSION_1[@]}
+if [[ ${#IDENTIFIERS_VERSION_2[@]} -gt $MAX_LEN ]]; then
+    MAX_LEN=${#IDENTIFIERS_VERSION_2[@]}
+fi
+
+# Compare each corresponding part
+for ((i = 0; i < MAX_LEN; i++)); do
+    # Default to 0 if index doesn't exist
+    v1_part=${IDENTIFIERS_VERSION_1[i]:-0}
+    v2_part=${IDENTIFIERS_VERSION_2[i]:-0}
+
+    if (( v1_part > v2_part )); then
+        echo "$1"
         exit 0
-
-    elif (( ${IDENTIFIERS_VERSION_2[$i]} > ${IDENTIFIERS_VERSION_1[$i]} )); then
-        echo $2
+    elif (( v2_part > v1_part )); then
+        echo "$2"
         exit 0
     fi
 done
 
-exit 1
+# If all parts equal after normalization
+echo "$1"
+exit 0
