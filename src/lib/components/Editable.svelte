50fbadca30a9e9edae5fb62f6b5c11b7e2d54c68
TimSalokat <Tim.salokat@outlook.com>
2025-09-12 15:37:20 +0300

    [skip ci] BUGFIX: moved editable styling to a global place

--- src/lib/components/Editable.svelte
+++ src/lib/components/Editable.svelte
@@ -32,29 +32,3 @@
         {value == "" ? placeholder : value}
     </p>
 {/if}
-
-<style>
-    :global(.__editable_element) {
-        background-color: transparent;
-        color: var(--text-color);
-        cursor: text;
-        border: 1px solid transparent;
-        box-sizing: border-box;
-        width: 100%;
-        resize: none;
-    }
-
-    :global(.__editable_element.empty) {
-        color: var(--text-color-muted);
-        opacity: .5;
-    }
-
-    :global(textarea.__editable_element), :global(input.__editable_element) {
-        border-bottom: 1px solid var(--primary-color);
-    }
-
-    :global(textarea.__editable_element:focus), :global(input.__editable_element:focus){
-        outline: none;
-    }
-
-</style>
